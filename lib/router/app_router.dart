4b1ba73759ab258f8b29c34083b8842168dcb86a
galortega <galfeort@espol.edu.ec>
2023-02-24 11:25:09 -0500

    vista de login screen

---
+++ lib/router/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:driver_app/screens/screens.dart';
+
+class AppRoutes {
+  static const initialRoute = 'login';
+
+  static Map<String, Widget Function(BuildContext)> routes = {
+    'login': (BuildContext context) => LoginPage(),
+  };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(
+      builder: (context) => const AlertScreen(),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:driver_app/screens/login_screen.dart';
+export 'package:driver_app/screens/alert_screen.dart';
